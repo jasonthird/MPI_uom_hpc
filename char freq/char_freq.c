e26a7b74656ccd663a9e9d4aee08377178ba9cc0
Iason Malkotsis <imalkotsis@gmail.com>
2023-05-03 05:54:03 +0300

    Create char_freq.c

---
+++ char freq/char_freq.c
@@ -0,0 +1,90 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include "mpi.h"
+
+#define N 128
+#define base 0
+
+int main (int argc, char *argv[]) {
+    
+    MPI_Init( &argc, &argv );
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    
+    char * buffer;
+    long file_size;
+    long i, j,*freq;
+
+    if (rank==0){
+        FILE *pFile;
+        char * filename;
+        size_t result;
+
+        if (argc != 2) {
+            printf ("Usage : %s <file_name>\n", argv[0]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+            return 1;
+        }
+        filename = argv[1];
+        pFile = fopen ( filename , "rb" );
+        if (pFile==NULL) {
+            printf ("File error\n"); 
+            MPI_Abort(MPI_COMM_WORLD, 2);
+            return 2;
+        }
+
+        
+
+        // obtain file size:
+        fseek (pFile , 0 , SEEK_END);
+        file_size = ftell (pFile);
+        rewind (pFile);
+        printf("file size is %ld\n", file_size);
+        
+        // allocate memory to contain the file:
+        buffer = (char*) malloc (sizeof(char)*file_size);
+
+        // copy the file into the buffer:
+        result = fread (buffer,1,file_size,pFile);
+
+        MPI_Bcast(&file_size, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+        
+        //scatter the array
+        MPI_Scatter(buffer, file_size/size, MPI_CHAR, buffer, file_size/size, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+        //close file
+        fclose (pFile);
+
+    }
+    else{
+
+        MPI_Bcast(&file_size, 1, MPI_LONG, 0, MPI_COMM_WORLD);
+        buffer = (char*) malloc (sizeof(char)*file_size/size);
+
+        MPI_Scatter(buffer, file_size/size, MPI_CHAR, buffer, file_size/size, MPI_CHAR, 0, MPI_COMM_WORLD);
+        
+    }
+
+    freq = (long*) malloc (sizeof(long)*N);
+    memset(freq, 0, N*sizeof(long));
+
+    //count the frequency
+	for (i=0; i<file_size; i++){
+		freq[buffer[i] - base]++;
+	}
+
+    //reduce the frequency
+    MPI_Reduce(&freq, &freq, N, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    //print the frequency
+    if (rank==0){
+    	for (j=0; j<N; j++){
+    		printf("%ld = %ld\n", j+base, freq[j]);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
